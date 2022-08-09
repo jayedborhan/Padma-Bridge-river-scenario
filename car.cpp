2dba99b4abcf0e09b3c82c82f073fd5df3dd5359
MD Borhan Uddin <102826817+jayedborhan@users.noreply.github.com>
2022-08-10 04:03:04 +0600

    Update car.cpp

--- car.cpp
+++ car.cpp
@@ -231,7 +231,7 @@ glutInit(&argc, argv);
 glutInitDisplayMode (GLUT_SINGLE | GLUT_RGB);
 glutInitWindowSize (1800, 900);
 glutInitWindowPosition (100, 150);
-glutCreateWindow ("Natural View of a car");
+glutCreateWindow ("Natural View of cars");
 glutDisplayFunc(myDisplay);
 
 myInit ();
